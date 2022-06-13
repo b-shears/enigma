08236bcd2d83969ba2fd04909e669c058da55eeb
Bryan Shears <brshears@gmail.com>
2022-06-12 19:08:11 -0600

    Create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,16 +4,34 @@ require './lib/shifts'
 
 class Enigma
   attr_reader :alphabet
+
   def initialize
     @alphabet = ("a".."z").to_a << " "
+
   end
 
   def todays_date
     Date.today.strftime("%d%m%y")
   end
 
-  def encrpyt
-
+  def encrypt(message, key, date)
+    @key = KeyGenerator.random_number
+    @shifts = Shifts.new(@key, todays_date)
+    encrypted_message(message, @shifts)
   end
 
-end
+  def encrypted_message(message, shifts)
+    message.chars.each_with_index.map do |letter, index|
+      if @alphabet.index(letter).nil?
+         letter
+      else
+        x = index % 4
+        y = shifts.shifts_sum.values[x]
+        z = (y + @alphabet.index(letter)) % 27
+        @alphabet[z]
+      end
+    end.join
+  end
+
+
+  end
